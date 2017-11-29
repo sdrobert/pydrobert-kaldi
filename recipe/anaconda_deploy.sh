d9f4d81b1ee916d45697a7046a51bcaffc0ece75
Sean Robertson <sdrobert@cs.toronto.edu>
2017-11-29 13:27:32 -0500

    More mucking around

--- recipe/anaconda_deploy.sh
+++ recipe/anaconda_deploy.sh
@@ -5,4 +5,4 @@
 # else
 #   conda build recipe -m recipe/travis_py3_conda_build_config.yaml
 # fi
-anaconda -t ${ANACONDA_TOKEN} upload -u sdrobert --register ${HOME}/miniconda/conda-bld/pydrobert-kaldi-*.tar.bz2
+anaconda -t ${ANACONDA_TOKEN} upload -u sdrobert --register ${HOME}/miniconda/conda-bld/*/pydrobert-kaldi-*.tar.bz2
