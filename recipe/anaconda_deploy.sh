73cc2b0a863cf9bb994ead57cedfedd70a34ac87
Sean Robertson <sdrobert@cs.toronto.edu>
2017-11-29 18:39:21 -0500

    I think we've got it

--- recipe/anaconda_deploy.sh
+++ recipe/anaconda_deploy.sh
@@ -4,12 +4,8 @@
 
 if [ "$1" = "dev" ]; then
   label=dev
-  version=dev
 elif [ "$1" = "main" ]; then
   label=main;
-  # GIT_DESCRIBE_TAG isn't guaranteed otherwise
-  git fetch --unshallow || exit 1
-  version=$(git describe --tags | cut -d- -f 1 | sed 's/v//')
   if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
     conda build recipe -m recipe/travis_py2_conda_build_config.yaml
   else
@@ -21,5 +17,4 @@ anaconda -t ${ANACONDA_TOKEN} upload \
   -u sdrobert \
   --register \
   ${HOME}/miniconda/conda-bld/*/pydrobert-kaldi-*.tar.bz2 \
-  -l $label \
-  -v $version
+  -l $label
