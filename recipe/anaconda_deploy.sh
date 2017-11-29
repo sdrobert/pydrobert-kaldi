b0e276d7aee222b52940f568d089c30bdd4c8c7f
Sean Robertson <sdrobert@cs.toronto.edu>
2017-11-29 12:14:21 -0500

    Mucking around with CI again

--- recipe/anaconda_deploy.sh
+++ recipe/anaconda_deploy.sh
@@ -2,11 +2,9 @@
 
 source deactivate || true
 
-conda build purge
-
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  conda build recipe -m recipe/travis_py2_conda_build_config.yaml
-else
-  conda build recipe -m recipe/travis_py3_conda_build_config.yaml
-fi
+# if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+#   conda build recipe -m recipe/travis_py2_conda_build_config.yaml
+# else
+#   conda build recipe -m recipe/travis_py3_conda_build_config.yaml
+# fi
 anaconda -t ${ANACONDA_TOKEN} upload -u sdrobert --register ${HOME}/miniconda/conda-bld/pydrobert-kaldi-*.tar.bz2
