60fd745934c7b0be7e6062305731ae6a9d510e18
Sean Robertson <sdrobert@cs.toronto.edu>
2017-12-06 21:04:52 -0500

    nomkl should work again. I keep deleting the runtime dependency...

--- recipe/travis_deploy.sh
+++ recipe/travis_deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+set -e
+
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml || exit 1
+  conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml
 else
-  conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml || exit 1
+  conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml
 fi
 
 anaconda -t ${ANACONDA_TOKEN} upload \
