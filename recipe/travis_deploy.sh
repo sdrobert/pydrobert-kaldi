accd8c1667b2b63fa362e9cfb7ef37a97a401dcf
Sean Robertson <sdrobert@cs.toronto.edu>
2017-12-07 13:19:28 -0500

    Hmm

--- recipe/travis_deploy.sh
+++ recipe/travis_deploy.sh
@@ -6,14 +6,11 @@ echo "From travis deploy: ${PY_VER} $1"
 
 if [[ "${PY_VER}" == "2.7" ]]; then
   echo "In py2 block"
-  # conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml
+  conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml
 else
   echo "In py3 block"
-  # conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml
+  conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml
 fi
-
-sleep 1
-exit 1
 
 anaconda -t ${ANACONDA_TOKEN} upload \
   -u sdrobert \
