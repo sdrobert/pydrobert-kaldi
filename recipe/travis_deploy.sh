5e33c0bc2f78de14babf5544592313157faf7948
Sean Robertson <sdrobert@cs.toronto.edu>
2017-12-06 20:21:34 -0500

    Travis is not sourcing properly, I think

--- recipe/travis_deploy.sh
+++ recipe/travis_deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml
+  conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml || exit 1
 else
-  conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml
+  conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml || exit 1
 fi
 
 anaconda -t ${ANACONDA_TOKEN} upload \
