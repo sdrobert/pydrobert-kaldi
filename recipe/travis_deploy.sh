b398ca1c77c3f5e7bd1443f02f6f03a377bed108
Sean Robertson <sdrobert@cs.toronto.edu>
2017-12-07 12:56:22 -0500

    Why won't these environment variables work?

--- recipe/travis_deploy.sh
+++ recipe/travis_deploy.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+echo "From travis deploy: ${PY_VER} $1"
+
 if [[ "${PY_VER}" == "2.7" ]]; then
   conda build recipe --skip-existing -m recipe/ci_py2_deploy.yaml
 else
   conda build recipe --skip-existing -m recipe/ci_py3_deploy.yaml
 fi
+
+sleep 1
+exit 1
 
 anaconda -t ${ANACONDA_TOKEN} upload \
   -u sdrobert \
