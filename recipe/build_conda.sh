7ff242d1015aab16c758ccd04dfd38b34115c6eb
Sean Robertson <sdrobert@cs.toronto.edu>
2019-12-04 14:40:12 -0500

    a

--- recipe/build_conda.sh
+++ recipe/build_conda.sh
@@ -12,9 +12,12 @@ shift
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda install conda-build conda-verify
-"CONDA_SUBDIR=$(basename "${dist_dir}")" conda build "${recipe_dir}" \
+_old="${CONDA_SUBDIR}"
+export CONDA_SUBDIR="${CONDA_SUBDIR:-$(basename "${dist_dir}")}"
+conda build "${recipe_dir}" \
   --python "${PY_VER}" \
   -m "${recipe_dir}/ci_build.yaml" "$@"
+export CONDA_SUBDIR="${_old}"
 
 "$(conda info --base)/bin/python" \
   "${recipe_dir}/copy_conda_build_packages.py" \
