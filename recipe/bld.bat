199daef1899c5ed7cc0fcef32f81ace7cb46ac41
Sean Robertson <sdrobert@cs.toronto.edu>
2021-02-16 20:00:06 -0500

    herglebergle

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,8 +21,10 @@ set MKLROOT=%LIBRARY_PREFIX%
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
 
 set LIBPATH=%LIBRARY_PREFIX%\bin
-echo "Python=%PYTHON% MKLROOT=%MKLROOT% SETUPTOOLS_SCM_PRETEND_VERSION=%SETUPTOOLS_SCM_PRETEND_VERSION%"
-"%PYTHON%" setup.py install ^
+
+%PYTHON% --version
+
+%PYTHON% setup.py install ^
   --single-version-externally-managed ^
   --record=record.txt ^
   --verbose
