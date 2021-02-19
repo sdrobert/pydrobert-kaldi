e0361e91832ffa2f2ce5835fc7f3746d59b377cf
Sean Robertson <sdrobert@cs.toronto.edu>
2021-02-19 12:15:25 -0500

    Go

--- recipe/cibw_before_build_manylinux.sh
+++ recipe/cibw_before_build_manylinux.sh
@@ -14,7 +14,7 @@ yum install -y curl || true
 tmpdir=$(mktemp -d)
 pushd $tmpdir
 curl -L https://github.com/swig/swig/archive/rel-4.0.2.tar.gz > rel-4.0.2
-[ "$(md5sum rel-4.0.2 | cut -d' ' -f 1)" = "77026bde998521cf1244fb43950203b9" ]
+[ "$(md5sum rel-4.0.2 | cut -d' ' -f 1)" = "19a61126f0f89c56b2c2e9e39cc33efe" ]
 tar -xf rel-4.0.2
 cd swig-rel-4.0.2
 ./autogen.sh --without-alllang --with-python
