172ea115880c9898f2f6bad9d3e69610fe9c24ae
Sean Robertson <sdrobert@cs.toronto.edu>
2021-02-19 11:27:14 -0500

    Let's try it

--- recipe/cibw_before_build_manylinux.sh
+++ recipe/cibw_before_build_manylinux.sh
@@ -10,19 +10,19 @@ fi
 
 yum install -y curl || true
 
-# build swig 3.0.8
-# tmpdir=$(mktemp -d)
-# pushd $tmpdir
-# curl -L https://github.com/swig/swig/archive/rel-3.0.8.tar.gz > rel-3.0.8
-# [ "$(md5sum rel-3.0.8 | cut -d' ' -f 1)" = "9b5862b1d782b111d87fce9216a2d465" ]
-# tar -xf rel-3.0.8
-# cd swig-rel-3.0.8
-# ./autogen.sh --without-alllang --with-python
-# ./configure --without-pcre
-# make
-# make install
-# popd
+# build swig 4.0.2
+tmpdir=$(mktemp -d)
+pushd $tmpdir
+curl -L https://github.com/swig/swig/archive/rel-4.0.2.tar.gz > rel-4.0.2
+[ "$(md5sum rel-4.0.2 | cut -d' ' -f 1)" = "77026bde998521cf1244fb43950203b9" ]
+tar -xf rel-4.0.2
+cd swig-rel-4.0.2
+./autogen.sh --without-alllang --with-python
+./configure --without-pcre
+make
+make install
+popd
 
-yum install -y swig-4.0.2 openblas-devel${MARCH_SUFFIX}
+yum install -y openblas-devel${MARCH_SUFFIX}
 
 pip install -r recipe/cibw_before_requirements.txt
