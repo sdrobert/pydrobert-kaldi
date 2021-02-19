716346f2d4763aec8a9e1dc129d78b7b899d7446
Sean Robertson <sdrobert@cs.toronto.edu>
2021-02-19 10:57:07 -0500

    Updating swig, mebbeh

--- recipe/cibw_before_build_manylinux.sh
+++ recipe/cibw_before_build_manylinux.sh
@@ -11,18 +11,18 @@ fi
 yum install -y curl || true
 
 # build swig 3.0.8
-tmpdir=$(mktemp -d)
-pushd $tmpdir
-curl -L https://github.com/swig/swig/archive/rel-3.0.8.tar.gz > rel-3.0.8
-[ "$(md5sum rel-3.0.8 | cut -d' ' -f 1)" = "9b5862b1d782b111d87fce9216a2d465" ]
-tar -xf rel-3.0.8
-cd swig-rel-3.0.8
-./autogen.sh --without-alllang --with-python
-./configure --without-pcre
-make
-make install
-popd
+# tmpdir=$(mktemp -d)
+# pushd $tmpdir
+# curl -L https://github.com/swig/swig/archive/rel-3.0.8.tar.gz > rel-3.0.8
+# [ "$(md5sum rel-3.0.8 | cut -d' ' -f 1)" = "9b5862b1d782b111d87fce9216a2d465" ]
+# tar -xf rel-3.0.8
+# cd swig-rel-3.0.8
+# ./autogen.sh --without-alllang --with-python
+# ./configure --without-pcre
+# make
+# make install
+# popd
 
-yum install -y openblas-devel${MARCH_SUFFIX}
+yum install -y swig-4.0.2 openblas-devel${MARCH_SUFFIX}
 
 pip install -r recipe/cibw_before_requirements.txt
