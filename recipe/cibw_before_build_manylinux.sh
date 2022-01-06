943c00e51ef6ffcd8018893c82022b58b62ceae5
Sean Robertson <sdrobert@cs.toronto.edu>
2022-01-06 13:26:35 -0500

    Delete conda stuff + try again

--- recipe/cibw_before_build_manylinux.sh
+++ recipe/cibw_before_build_manylinux.sh
@@ -30,7 +30,7 @@ fi
 
 if ! pkg-config --exists 'openblas'; then
   yum install -y openblas-devel${MARCH_SUFFIX}
-  pkg-config --print-variables 'openblas'
+  rpm -q openblas-devel${MARCH_SUFFIX}
 fi
 
 python -m pip install -r recipe/cibw_before_requirements.txt
--- recipe/cibw_before_build_win.ps1
+++ recipe/cibw_before_build_win.ps1
@@ -16,14 +16,14 @@ $lapackeh = Get-ChildItem -Path $env:OPENBLASROOT -Recurse -Filter "lapacke.h" -
 
 if (($null -eq $openblaslib) -or ($null -eq $cblash) -or ($null -eq $lapackeh)) {
   Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/openblas/0.2.20/download/win-64/openblas-0.2.20-vc14_8.tar.bz2" -OutFile "openblas.tar.bz2"
-  $hash = Get-FileHash openblas.tar.bz2
-  if (-not ($hash -eq "74BB55BCC4C5B760A08424ED7A53D08FF9581278BB05441F7F6E5F43AADCF8CA")) { Write-Error "opemblas hash does not match" }
+  $hash = Get-FileHash openblas.tar.bz2 -
+  if (-not ($hash.Hash -eq "74BB55BCC4C5B760A08424ED7A53D08FF9581278BB05441F7F6E5F43AADCF8CA")) { Write-Error "openblas hash does not match" }
   & 7z x openblas.tar.bz2
   & 7z x -aoa openblas.tar
   if (-not $?) { Write-Error "Unable to extract openblas" }
   Invoke-WebRequest -Uri "https://anaconda.org/conda-forge/libflang/5.0.0/download/win-64/libflang-5.0.0-vc14_1.tar.bz2" -OutFile "libflang.tar.bz2"
   $hash = Get-FileHash libflang.tar.bz2
-  if (-not ($hash -eq "BC50A67898F82820F5138938FBAABC90CF13495CE188E91725A33B6FAD25F06F")) { Write-Error "libflang hash does not match" }
+  if (-not ($hash.Hash -eq "BC50A67898F82820F5138938FBAABC90CF13495CE188E91725A33B6FAD25F06F")) { Write-Error "libflang hash does not match" }
   & 7z x libflang.tar.bz2
   & 7z x -aoa libflang.tar
   if (-not $?) { Write-Error "Unable to extract libflang" }
